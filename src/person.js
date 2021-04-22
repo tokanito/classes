106bd278ac3cdec665f9993ec7a92d978a422732
tokanito <78441278+tokanito@users.noreply.github.com>
2021-04-22 13:28:03 +0400

    fix:
    fix count

--- src/person.js
+++ src/person.js
@@ -3,13 +3,7 @@ export class Person {
     constructor(surname,name){
         this.surname = surname;
         this.name = name;
-        Person.instances+=1;
-        // this.addcount=function(){
-        //     var count=0;
-        //     return count;
-        // }
-        this.count=1;
-        // this.arr=new Set(this);
+        Person.instances+=1;     
        
     }
     show (){
